fcba3233eff8055acb2c9ef56ee0c2e5a3f36ba9
Bobby Prabowo <prabowo.bobby@gmail.com>
2009-12-24 06:13:09 +0700

    added show news navigations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def current_year
+    Time.now.strftime('%Y')
+  end
+  
+  def truncate_words(text, length = 30, end_string = ' …')
+  return if text == nil
+  words = text.split()
+  words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
+  
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,5 @@
 class News < ActiveRecord::Base
   attr_accessible :title, :content
+  
+  acts_as_list :column => "id"
 end

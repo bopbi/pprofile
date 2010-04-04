49a59647c8962571c7117e0198a4ff7d34cb3fbd
Bobby Prabowo <prabowo.bobby@gmail.com>
2010-04-04 12:48:51 +0700

    fix 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,26 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     render :template => "errors/error404", :status => "404"
   end
+  
+  def rescue_404
+    rescue_action_in_public(ActionController::RoutingError)
+  end
+  
+  def rescue_action_in_public(exception)
+    #maybe gather up some data you'd want to put in your error page
+  
+    case exception
+      when ActionController::InvalidAuthenticityToken
+      when ArgumentError
+      when SyntaxError
+        render :template => "errors/error404", :status => "404"
+      else
+        render :template => "errors/error500", :status => "500"
+    end          
+  end
+
+  def local_request?
+    return false
+  end
+  
 end
